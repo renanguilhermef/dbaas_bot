976b266865f259047b4d58bc19b041b6364f9ba7
renan.guilherme.de.francisco <renan.guilherme.de.francisco@ing.com>
2026-02-08 02:29:57 +0100

    new files

--- deploy/setup_dbaas_bot.sh
+++ deploy/setup_dbaas_bot.sh
@@ -3,6 +3,6 @@ apt-get update
 apt-get install -y docker.io docker-compose git
 usermod -aG docker $USER
 cd /home/${USER}
-git clone https://github.com/YOUR_GITHUB_USER/YOUR_REPO.git dbaas_bot
+git clone https://github.com/renanguilhermef/dbaas_bot.git dbaas_bot
 cd dbaas_bot
-docker compose up -d --build
+docker-compose up -d --build
